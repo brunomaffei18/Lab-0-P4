34f82401cbb9ca27d205a55baffe27ac820d337e
Ariel Ignacio Falcon Marroni - ENS <ariel.falcon@pcunix31.fing.edu.uy>
2025-03-24 15:46:27 -0300

    SoftUpdate

--- Implementacion/DTRefer.cpp
+++ Implementacion/DTRefer.cpp
@@ -10,3 +10,15 @@ DTRefer :: DTRefer(string DOI, string titulo, DTFecha fecha){
     this->autores = vector<string>();
 }
 
+ostream& operator<<(ostream& os, DTRefer& dt) {
+    os << dt.getDOI() << "->" << dt.getTitulo() << " (" << dt.getFecha() << ") /";
+    
+    vector<string> autores = dt.getAutores();  // Usamos el getter de autores
+    for (size_t i = 0; i < autores.size(); i++) {
+        os << autores[i];
+        if (i < autores.size() - 1) {
+            os << ", ";  // Separador entre autores
+        }
+    }
+    return os;  // Retornamos el flujo modificado
+}
