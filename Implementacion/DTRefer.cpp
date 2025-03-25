03638e3be591e096dfd50d6a6577a82201e6e369
Ariel Ignacio Falcon Marroni - ENS <ariel.falcon@pcunix26.fing.edu.uy>
2025-03-25 10:18:53 -0300

    DeBug

--- Implementacion/DTRefer.cpp
+++ Implementacion/DTRefer.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 
 
-DTRefer :: DTRefer(string DOI, string titulo, DTFecha fecha){
+DTRefer :: DTRefer(string DOI, string titulo,DTFecha fecha){
     this->fecha = fecha;
     this->DOI = DOI;
     this->titulo = titulo;
