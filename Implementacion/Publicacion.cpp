e4413117c2a3c400653f08b1c3c9146a17c1fc94
root <root@DESKTOP-5J5P3LQ.>
2025-03-26 14:19:52 -0300

    gestion del map misInvestigadores

--- Implementacion/Publicacion.cpp
+++ Implementacion/Publicacion.cpp
@@ -22,11 +22,20 @@ DTRefer Publicacion::getDT(){
     std::vector<std::string>autores;
     for (auto i = misInvestigadores.begin(); i != misInvestigadores.end(); i++)
     {
-        autores.push_back(i->first->getNombre());
+        autores.push_back(i->second->getNombre());
     }
     
     DTRefer Refer = DTRefer(DOI,titulo,fecha,autores);
     return Refer;
+}
+
+void Publicacion::setMapInve(string orcidInve, Investigador* Inv){
+    misInvestigadores.insert(std::pair<std::string, Investigador*>(orcidInve, Inv));
+}
+
+void Publicacion::unSetMapInve(string orcidInve){
+    misInvestigadores.erase(orcidInve);
+
 }
 
 //No se implelemnta contien Palabara
