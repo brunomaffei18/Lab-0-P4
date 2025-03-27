cb057986bbd82915e2076542bdbeb50628fb2be2
Martín Pedragosa Ruiz Diaz - ENS <martin.pedragosa@pcunix141.fing.edu.uy>
2025-03-27 14:14:17 -0300

    cambios en la funcion contienePalabra de PaginaWeb y de articulosRevista

--- Implementacion/PaginaWeb.cpp
+++ Implementacion/PaginaWeb.cpp
@@ -20,11 +20,50 @@
     return this->url;
   }
 
+bool comparar(string contenido, int pos,int ne ,string palabra,int np ){
+   int  i=0;
+   bool igual=true;
+    while (pos<ne && i<np &&igual)
+    {
+        if (contenido[pos]!=palabra[i])
+        {
+            igual=false;
+        }
+        pos++;
+        i++;
+        
+    }
+    if(pos-ne>0 && contenido[pos-np-1]==' '){
+    
+        igual=false;
+      
+    }
+if (pos<ne && contenido[pos]==' ')
+{
+    igual=false;
+}
+    return igual;
+
+}
 
   //funciones
-    bool PaginaWeb::contienePalabra(std::string palabra){
-        return contenido.find(palabra)!=string::npos;
+  bool PaginaWeb::contienePalabra(std::string palabra){
+       int largo=contenido.size();
+int palabralargo=palabra.size();
+bool esta=false; 
+for (int i = 0; i < largo && !esta; i++)
+{
+    if(contenido[i]==palabra[0]){
+     esta=comparar(contenido,i,largo,palabra,palabralargo);
     }
+}
+
+
+
+    return esta;
+
+}
+    
 
 
 
